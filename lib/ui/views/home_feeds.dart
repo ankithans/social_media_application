0840210ef814f55bcfcf8d3854c239fea2f0c62e
ankithans <ankithans1947@gmail.com>
2020-06-19 11:47:45 +0530

    Home Screen UI Completed

---
+++ lib/ui/views/home_feeds.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:social_media_application/models/models.dart';
+import 'package:social_media_application/ui/widgets/avatar_widget.dart';
+import 'package:social_media_application/ui/widgets/post_widget.dart';
+
+class HomeFeedPage extends StatefulWidget {
+  final ScrollController scrollController;
+
+  HomeFeedPage({this.scrollController});
+
+  @override
+  _HomeFeedPageState createState() => _HomeFeedPageState();
+}
+
+class _HomeFeedPageState extends State<HomeFeedPage> {
+  final _posts = <Post>[
+    Post(
+      user: grootlover,
+      imageUrls: [
+        'https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg',
+        'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+        'https://cdn.pixabay.com/photo/2015/02/24/15/41/dog-647528__340.jpg',
+      ],
+      likes: [
+        Like(user: rocket),
+        Like(user: starlord),
+        Like(user: gamora),
+        Like(user: nickwu241),
+      ],
+      comments: [
+        Comment(
+          text: 'So we’re saving the galaxy again? #gotg',
+          user: rocket,
+          commentedAt: DateTime(2019, 5, 23, 14, 35, 0),
+          likes: [Like(user: nickwu241)],
+        ),
+      ],
+      location: 'Earth',
+      postedAt: DateTime(2019, 5, 23, 12, 35, 0),
+    ),
+    Post(
+      user: nickwu241,
+      imageUrls: [
+        'https://cdn.pixabay.com/photo/2015/02/24/15/41/dog-647528__340.jpg'
+      ],
+      likes: [],
+      comments: [],
+      location: 'Knowhere',
+      postedAt: DateTime(2019, 5, 21, 6, 0, 0),
+    ),
+    Post(
+      user: nebula,
+      imageUrls: [
+        'https://cdn.pixabay.com/photo/2015/02/24/15/41/dog-647528__340.jpg'
+      ],
+      likes: [Like(user: nickwu241)],
+      comments: [],
+      location: 'Nine Realms',
+      postedAt: DateTime(2019, 5, 2, 0, 0, 0),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: BouncingScrollPhysics(),
+      itemBuilder: (ctx, i) {
+        if (i == 0) {
+          return StoriesBarWidget();
+        }
+        return PostWidget(_posts[i - 1]);
+      },
+      itemCount: _posts.length + 1,
+      controller: widget.scrollController,
+    );
+  }
+}
+
+class StoriesBarWidget extends StatelessWidget {
+  final _users = <User>[
+    currentUser,
+    grootlover,
+    rocket,
+    nebula,
+    starlord,
+    gamora,
+  ];
+
+  void _onUserStoryTap(BuildContext context, int i) {
+    final message =
+        i == 0 ? 'Add to Your Story' : "View ${_users[i].name}'s Story";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 106.0,
+      child: ListView.builder(
+        physics: BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (ctx, i) {
+          return AvatarWidget(
+            user: _users[i],
+            onTap: () => _onUserStoryTap(context, i),
+            isLarge: true,
+            isShowingUsernameLabel: true,
+            isCurrentUserStory: i == 0,
+          );
+        },
+        itemCount: _users.length,
+      ),
+    );
+  }
+}
---
+++ lib/ui/views/home_page.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+import 'package:outline_material_icons/outline_material_icons.dart';
+import 'package:social_media_application/ui/views/home_feeds.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  static const _kAddPhotoTabIndex = 2;
+  int _tabSelectedIndex = 0;
+
+  // Save the home page scrolling offset,
+  // used when navigating back to the home page from another tab.
+  double _lastFeedScrollOffset = 0;
+  ScrollController _scrollController;
+
+  @override
+  void dispose() {
+    _disposeScrollController();
+    super.dispose();
+  }
+
+  void _scrollToTop() {
+    if (_scrollController == null) {
+      return;
+    }
+    _scrollController.animateTo(
+      0.0,
+      duration: Duration(milliseconds: 250),
+      curve: Curves.decelerate,
+    );
+  }
+
+  // Call this when changing the body that doesn't use a ScrollController.
+  void _disposeScrollController() {
+    if (_scrollController != null) {
+      _lastFeedScrollOffset = _scrollController.offset;
+      _scrollController.dispose();
+      _scrollController = null;
+    }
+  }
+
+  Widget _buildPlaceHolderTab(String tabName) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 64.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Oops, the $tabName tab is\n under construction!',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 28.0),
+            ),
+            Image.asset('assets/images/building.gif'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBody() {
+    switch (_tabSelectedIndex) {
+      case 0:
+        _scrollController =
+            ScrollController(initialScrollOffset: _lastFeedScrollOffset);
+        return HomeFeedPage(scrollController: _scrollController);
+      default:
+        const tabIndexToNameMap = {
+          0: 'Home',
+          1: 'Search',
+          2: 'Add Photo',
+          3: 'Notifications',
+          4: 'Profile',
+        };
+        _disposeScrollController();
+        return _buildPlaceHolderTab(tabIndexToNameMap[_tabSelectedIndex]);
+    }
+  }
+
+  // Unselected tabs are outline icons, while the selected tab should be solid.
+  Widget _buildBottomNavigation() {
+    const unselectedIcons = <IconData>[
+      OMIcons.home,
+      OMIcons.search,
+      Icons.add_circle_outline,
+      Icons.notifications_none,
+      Icons.person_outline,
+    ];
+    const selecteedIcons = <IconData>[
+      Icons.home,
+      Icons.search,
+      Icons.add_circle,
+      Icons.notifications,
+      Icons.person,
+    ];
+    final bottomNaivgationItems = List.generate(5, (int i) {
+      final iconData =
+          _tabSelectedIndex == i ? selecteedIcons[i] : unselectedIcons[i];
+      return BottomNavigationBarItem(
+        icon: Icon(iconData),
+        title: Container(),
+      );
+    }).toList();
+
+    return Builder(builder: (BuildContext context) {
+      return BottomNavigationBar(
+        iconSize: 28.0,
+        selectedIconTheme: IconThemeData(
+          color: Colors.black,
+          size: 32,
+        ),
+        type: BottomNavigationBarType.fixed,
+        items: bottomNaivgationItems,
+        currentIndex: _tabSelectedIndex,
+        onTap: (int i) => _onTabTapped(context, i),
+      );
+    });
+  }
+
+  static List<Widget> _widgetOptions = <Widget>[
+    HomeFeedPage(),
+    Text('Search'),
+    Text('add Post'),
+    Text('Notifications'),
+    Text('Profile'),
+  ];
+
+  void _onTabTapped(BuildContext context, int index) {
+    if (index == _kAddPhotoTabIndex) {
+    } else if (index == _tabSelectedIndex) {
+      _scrollToTop();
+    } else {
+      setState(() => _tabSelectedIndex = index);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1.0,
+        backgroundColor: Colors.grey[50],
+        title: Row(
+          children: <Widget>[
+            Builder(builder: (BuildContext context) {
+              return GestureDetector(
+                child: Icon(OMIcons.cameraAlt, color: Colors.black, size: 32.0),
+                onTap: () => {},
+              );
+            }),
+            SizedBox(width: 12.0),
+            GestureDetector(
+              child: Text(
+                'Social Media',
+                style: TextStyle(
+                    fontFamily: 'Billabong',
+                    color: Colors.black,
+                    fontSize: 32.0),
+              ),
+              onTap: _scrollToTop,
+            ),
+          ],
+        ),
+        actions: <Widget>[
+          Builder(builder: (BuildContext context) {
+            return IconButton(
+              color: Colors.black,
+              icon: Icon(OMIcons.nearMe),
+              onPressed: () => {},
+            );
+          }),
+        ],
+      ),
+      body: _widgetOptions.elementAt(_tabSelectedIndex),
+      bottomNavigationBar: _buildBottomNavigation(),
+    );
+  }
+}
